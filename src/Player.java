4c166ffa8b0d835a115b70a1fd48413504bb3084
shuu <morijyobi>
2024-07-12 12:29:36 +0900

    CPuの手をランダムにする処理を実装したclose #14, close #15

--- src/Player.java
+++ src/Player.java
@@ -1,7 +1,35 @@
 package src;
+import java.util.Random;
 public class Player {
 
     private String playerName;
+    public void createRandomHand() {
+
+
+
+        Random random = new Random();
+
+        int randomValue = random.nextInt(3);
+
+
+
+        if(randomValue == 0) {
+
+            this.hand = new Gu();
+
+        } else if(randomValue == 1) {
+
+            this.hand = new Choki();
+
+        } else {
+
+            this.hand = new Pa();
+
+        }
+
+    }
+
+
 
     public Player(String playerName) {
 
